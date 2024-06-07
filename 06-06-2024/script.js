a133a187d2e54ed2dd3662ecbcf0f749236976bb
RobertaMi89 <roberta.miosi@gmail.com>
2024-06-07 10:17:04 +0200

    function refactoring

--- 06-06-2024/script.js
+++ 06-06-2024/script.js
@@ -43,13 +43,13 @@ showProducts.addEventListener("click", () => {
 
 //filtro dentro il campo input
 const searchInput = document.getElementById("search-input");
-searchInput.addEventListener("input", searchProducts);
 
-function searchProducts() {
-  let input = document.getElementById("search-input").value.toLowerCase();
-  let products = document.querySelectorAll(".card");
+searchInput.addEventListener("input", () => {
+  const input = searchInput.value.toLowerCase();
+  const products = document.querySelectorAll(".card");
+
   products.forEach((product) => {
     const title = product.querySelector("h3").textContent.toLowerCase();
     product.style.display = title.includes(input) ? "block" : "none";
   });
-}
+});
