7fbb38332da684197c1906d3100a087412fea8f9
RobertaMi89 <roberta.miosi@gmail.com>
2024-06-27 11:14:34 +0200

    edit menu footer

--- 26-06-2024/react-app/src/components/Footer/Footer.jsx
+++ 26-06-2024/react-app/src/components/Footer/Footer.jsx
@@ -1,15 +1,68 @@
 import "./Footer.css";
-import { MenuItem } from "../Header/Header";
 export function Footer() {
   return (
     <>
       <div className="footer-logo">
         <img src="../src/assets/logo.png" alt="logo apple" />
-        <MenuItem label={"Contact"} />
-        <MenuItem label={"Careers"} />
-        <MenuItem label={"Legal"} />
-        <MenuItem label={"United States"} />
+        <div className="container">
+          <div className="wrapper"></div>
+        </div>
+        <MenuList />
       </div>
     </>
+  );
+}
+function MenuList() {
+  const menus = [
+    {
+      id: 1,
+      label: "Shop and Learn",
+      items: [
+        { label: "Company info" },
+        { label: "Careers" },
+        { label: "Other Stuff" },
+      ],
+    },
+
+    {
+      id: 2,
+      label: "Account",
+      items: [
+        { label: "Company info" },
+        { label: "Careers" },
+        { label: "Other Stuff", items: [{ label: "Stuff" }] },
+      ],
+    },
+
+    {
+      id: 3,
+      label: "Account",
+    },
+  ];
+
+  return (
+    <ul className="menu">
+      {menus.map((menu) => (
+        <Menu key={menu.id} menu={menu} />
+      ))}
+    </ul>
+  );
+}
+
+function Menu(props) {
+  const { label, items = [], href = "#" } = props.menu;
+
+  return (
+    <li className="item">
+      <a href={href}>{label}</a>
+
+      {Boolean(items.length) && (
+        <ul>
+          {items.map((item, index) => (
+            <Menu key={index} menu={item} />
+          ))}
+        </ul>
+      )}
+    </li>
   );
 }
