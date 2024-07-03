fe01f7fe1a6b885b743886ea0e0c42c87e59dba2
RobertaMi89 <roberta.miosi@gmail.com>
2024-07-03 23:30:32 +0200

    style

--- 03-07-2024/react-form-app/src/App.jsx
+++ 03-07-2024/react-form-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Form from "./components/form/Form";
+
 import "./App.css";
 
 function App() {
--- 03-07-2024/react-form-app/src/components/form/Form.jsx
+++ 03-07-2024/react-form-app/src/components/form/Form.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-import "./form.module.css";
-
+import styles from "./form.module.css";
 function Form() {
   const [input, setInput] = useState({
     name: "",
@@ -50,66 +49,96 @@ function Form() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <h1>Contact US</h1>
-        <div>
-          <label>First Name *</label>
-          <input
-            name="name"
-            value={input.name}
-            onChange={handleChange}
-            className={errors.name ? "input-error" : ""}
-          />
-          {errors.name && <p style={{ color: "red" }}>{errors.name}</p>}
-          <label>Last Name *</label>
-          <input name="surname" value={input.surname} onChange={handleChange} />
-          {errors.surname && <p style={{ color: "red" }}>{errors.surname}</p>}
-          <label>Email *</label>
-          <input name="email" value={input.email} onChange={handleChange} />
-          {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
-          <label>Query Type *</label>
-          <fieldset>
-            <input
-              type="radio"
-              id="generalEnquiry"
-              name="queryType"
-              value="generalEnquiry"
-              onChange={handleChange}
-            />
-            <label htmlFor="generalEnquiry">General Enquiry</label>
-          </fieldset>
-          <fieldset>
-            <input
-              type="radio"
-              id="supportRequest"
-              name="queryType"
-              value="supportRequest"
-              onChange={handleChange}
-            />
-            <label htmlFor="supportRequest">Support Request</label>
-          </fieldset>
-          {errors.queryType && (
-            <p style={{ color: "red" }}>{errors.queryType}</p>
-          )}
-          <label htmlFor="message">Message *</label>
-          <textarea
-            name="message"
-            id="message"
-            rows="4"
-            cols="50"
-            value={input.message}
-            onChange={handleChange}
-          />
-          {errors.message && <p style={{ color: "red" }}>{errors.message}</p>}
-          <input type="checkbox" /> I consent to being contacted by the team *
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-      <p>{input.name}</p>
-      <p>{input.surname}</p>
-      <p>{input.email}</p>
-      <p>{input.queryType}</p>
-      <p>{input.message}</p>
+      <div className={styles.containerWrapper}>
+        <form className="contact-form" onSubmit={handleSubmit}>
+          <h1>Contact US</h1>
+          <div>
+            <div className={styles.fullName}>
+              <div className={styles.name}>
+                <label>First Name *</label>
+                <br />
+                <input
+                  name="name"
+                  value={input.name}
+                  onChange={handleChange}
+                  className={errors.name ? "input-error" : ""}
+                />
+              </div>
+              {errors.name && <p style={{ color: "red" }}>{errors.name}</p>}
+              <div className={styles.surname}>
+                <label>Last Name *</label>
+
+                <br />
+                <input
+                  name="surname"
+                  value={input.surname}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            {errors.surname && <p style={{ color: "red" }}>{errors.surname}</p>}
+            <div className={styles.email}>
+              <label>Email *</label>
+              <input name="email" value={input.email} onChange={handleChange} />
+              {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
+            </div>
+            <div className={styles.queryLabel}>
+              <label>Query Type *</label>
+            </div>
+            <div className={styles.query}>
+              <br />
+              <fieldset className={styles.radio}>
+                <input
+                  type="radio"
+                  id="generalEnquiry"
+                  name="queryType"
+                  value="generalEnquiry"
+                  onChange={handleChange}
+                />
+                <label htmlFor="generalEnquiry">General Enquiry</label>
+              </fieldset>
+              <fieldset className={styles.radio}>
+                <input
+                  type="radio"
+                  id="supportRequest"
+                  name="queryType"
+                  value="supportRequest"
+                  onChange={handleChange}
+                />
+                <label htmlFor="supportRequest">Support Request</label>
+              </fieldset>
+              {errors.queryType && (
+                <p style={{ color: "red" }}>{errors.queryType}</p>
+              )}
+            </div>
+            <div className={styles.message}>
+              <label htmlFor="message">Message *</label>
+              <textarea
+                name="message"
+                id="message"
+                rows="4"
+                cols="50"
+                value={input.message}
+                onChange={handleChange}
+                className={errors.message ? "input-error" : ""}
+              />
+              {errors.message && (
+                <p style={{ color: "red" }}>{errors.message}</p>
+              )}
+            </div>
+            <input type="checkbox" name="check" />
+            <label htmlFor="name">
+              I consent to being contacted by the team *
+            </label>
+          </div>
+          <input type="submit" value="Submit" className={styles.submit} />
+        </form>
+        <p>{input.name}</p>
+        <p>{input.surname}</p>
+        <p>{input.email}</p>
+        <p>{input.queryType}</p>
+        <p>{input.message}</p>
+      </div>
     </>
   );
 }
