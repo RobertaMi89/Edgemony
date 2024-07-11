08749ee64e47b99ccbb090fd5be920043b7361c4
RobertaMi89 <roberta.miosi@gmail.com>
2024-07-11 17:27:08 +0200

    refactor

--- 11-07-2024/e-commerce/src/components/molecules/navbar/NavBar.jsx
+++ 11-07-2024/e-commerce/src/components/molecules/navbar/NavBar.jsx
@@ -1,14 +1,15 @@
-import React from "react";
 import styles from "./navbar.module.css";
 import IconCart from "../../atom/icons/IconCart";
 import Avatar from "../../../../ecommerce-product-page-main/images/image-avatar.png";
 import Logo from "../../atom/logo/Logo";
+import Menu from "../../atom/menu/Menu.jsx";
 function NavBar() {
   return (
     <>
       <nav>
         <div className={styles.menu}>
           <Logo />
+          <Menu />
         </div>
         <div className={styles.user}>
           <IconCart />
--- 11-07-2024/e-commerce/src/components/organism/main/Main.jsx
+++ 11-07-2024/e-commerce/src/components/organism/main/Main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ProductLightBox from "../../molecules/product-lightBox/ProductLightBox.jsx";
 import Description from "../../molecules/description/Description.jsx";
 import styles from "./main.module.css";
