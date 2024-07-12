cc66d6422dd50ed207e468e4f9dcf68ee81422d4
RobertaMi89 <roberta.miosi@gmail.com>
2024-07-12 18:55:21 +0200

    add icon

--- 12-07-2024/react-tailwind/src/App.jsx
+++ 12-07-2024/react-tailwind/src/App.jsx
@@ -3,7 +3,7 @@ import Equilibrium from "../nft-preview-card-component-main/images/image-equilib
 import IconEthereum from "./components/IconEthereum.jsx";
 import IconClock from "./components/IconClock.jsx";
 import Avatar from "../nft-preview-card-component-main/images/image-avatar.png";
-
+import IconView from "./components/IconView.jsx";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -14,12 +14,16 @@ function App() {
           className="font-sans p-8 max-w-lg rounded-2xl flex flex-col items-center bg-Verydarkbluecard w-96"
           href="#"
         >
-          <img
-            src={Equilibrium}
-            className="shadow rounded-lg overflow-hidden"
-          />
+          <div className="relative w-full h-full flex items-center justify-center">
+            <img src={Equilibrium} className="rounded-lg" />
+            <div className="absolute inset-0 bg-Cyan rounded-lg opacity-0 hover:opacity-50 transition-opacity duration-300 w-full h-full flex items-center justify-center">
+              <IconView className="opacity-100" />
+            </div>
+          </div>
           <div className="mt-5 w-full">
-            <h4 className="text-xl text-White">Equilibrium #3429</h4>
+            <h4 className="text-xl text-White cursor-pointer hover:text-Cyan">
+              Equilibrium #3429
+            </h4>
             <p className="mt-2 text-gray-600 text-Softblue">
               Our Equilibrium collection promotes balance and calm
             </p>
@@ -41,7 +45,7 @@ function App() {
                 alt="avatar"
               />
               <span className="text-Softblue mr-2">Creation of </span>
-              <span className="text-White"> Roberta Miosi</span>
+              <span className="text-White hover:text-Cyan"> Roberta Miosi</span>
             </div>
           </div>
         </a>
