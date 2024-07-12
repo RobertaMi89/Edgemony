173002a5a17ea86fa29d93e159a4acd1cb12bb8d
RobertaMi89 <roberta.miosi@gmail.com>
2024-07-12 17:51:10 +0200

    add card and style

--- 12-07-2024/react-tailwind/src/App.jsx
+++ 12-07-2024/react-tailwind/src/App.jsx
@@ -1,11 +1,51 @@
 import { useState } from "react";
+import Equilibrium from "../nft-preview-card-component-main/images/image-equilibrium.jpg";
+import IconEthereum from "./components/IconEthereum.jsx";
+import IconClock from "./components/IconClock.jsx";
+import Avatar from "../nft-preview-card-component-main/images/image-avatar.png";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="flex w-full h-screen items-center justify-center bg-Verydarkblue">
+        <a
+          className="font-sans p-8 max-w-lg rounded-2xl flex flex-col items-center bg-Verydarkbluecard w-96"
+          href="#"
+        >
+          <img
+            src={Equilibrium}
+            className="shadow rounded-lg overflow-hidden"
+          />
+          <div className="mt-5 w-full">
+            <h4 className="text-xl text-White">Equilibrium #3429</h4>
+            <p className="mt-2 text-gray-600 text-Softblue">
+              Our Equilibrium collection promotes balance and calm
+            </p>
+            <div className="mt-5 mb-5 flex justify-between w-full ">
+              <div className="flex items-center">
+                <IconEthereum />
+                <span className="ms-2 text-Cyan font-bold">0.041 ETH</span>
+              </div>
+              <div className="flex text-right items-center text-Softblue">
+                <IconClock />
+                <span className="ms-2">3 days left</span>
+              </div>
+            </div>
+            <div className="border-t border-Verydarkblueline"></div>
+            <div className="mt-5 flex items-center">
+              <img
+                src={Avatar}
+                className="border rounded-full w-10  mr-5"
+                alt="avatar"
+              />
+              <span className="text-Softblue mr-2">Creation of </span>
+              <span className="text-White"> Roberta Miosi</span>
+            </div>
+          </div>
+        </a>
+      </div>
     </>
   );
 }
--- 12-07-2024/react-tailwind/tailwind.config.js
+++ 12-07-2024/react-tailwind/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Softblue: `var(--Softblue)`,
+        Cyan: `var(--Cyan)`,
+        Verydarkblue: `var(--Verydarkblue)` /*(main BG)*/,
+        Verydarkbluecard: `var(--Verydarkbluecard)` /*(card BG)*/,
+        Verydarkblueline: `var(--Verydarkblueline)`,
+        White: `var(--White)`,
+      },
+      fontFamily: {
+        sans: ["Outfit", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
