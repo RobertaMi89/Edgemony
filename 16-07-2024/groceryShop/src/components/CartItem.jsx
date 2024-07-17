4f13e989258432d2ee8b139cde8d4b5476a2e623
RobertaMi89 <roberta.miosi@gmail.com>
2024-07-17 11:23:35 +0200

    fix img

--- 16-07-2024/groceryShop/src/components/CartItem.jsx
+++ 16-07-2024/groceryShop/src/components/CartItem.jsx
@@ -6,11 +6,7 @@ const CartItem = ({ item }) => {
 
   return (
     <div className="border rounded-lg shadow-lg p-4 flex flex-col items-center">
-      <img
-        src={item.image}
-        alt={item.title}
-        className="w-32 h-32 object-cover mb-4"
-      />
+      <img src={item.image} alt={item.title} className="w-32 mb-4" />
       <h2 className="text-xl font-semibold mb-2 text-center">{item.title}</h2>
       <p className="text-gray-700">${item.price}</p>
       <button
