67df6757e9a1a32199f65ac0e266dcf102af0448
RobertaMi89 <roberta.miosi@gmail.com>
2024-07-17 19:15:42 +0200

    add style

--- 17-07-2024/e-commerce-router/src/components/ProductItem.jsx
+++ 17-07-2024/e-commerce-router/src/components/ProductItem.jsx
@@ -5,8 +5,12 @@ const ProductItem = ({ product }) => {
   const { addToCart } = useContext(CartContext);
 
   return (
-    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 relative">
-      <img src={product.image} alt={product.title} className="rounded-t-lg" />
+    <div className="flex flex-wrap justify-center h-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 relative">
+      <img
+        src={product.image}
+        alt={product.title}
+        className="rounded-t-lg h-auto w-44 object-contain m-2"
+      />
       <div className="p-5">
         <h4 className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">
           {product.title}
@@ -15,10 +19,12 @@ const ProductItem = ({ product }) => {
           ${product.price}
         </p>
         <p className="mb-9 font-normal text-gray-700 dark:text-gray-400">
-          ${product.description}
+          {product.description}
         </p>
         <button
-          onClick={() => addToCart(product)}
+          onClick={() => {
+            addToCart(product);
+          }}
           className="bg-blue-500 text-white w-48 px-4 py-2  rounded hover:bg-blue-700 absolute bottom-2.5 left-2.5 "
         >
           Add to Cart
--- 17-07-2024/e-commerce-router/src/pages/Cart.jsx
+++ 17-07-2024/e-commerce-router/src/pages/Cart.jsx
@@ -6,14 +6,24 @@ const Cart = () => {
   const { cart } = useContext(CartContext);
 
   return (
-    <div className="container mx-auto my-8">
-      <h1 className="text-4xl font-bold text-center mb-8">Cart</h1>
-      <div className="grid grid-cols-1 gap-4">
-        {cart.map((item, index) => (
-          <CartItem key={index} item={item} />
-        ))}
+    <section class="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
+      <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
+        <h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+          Shopping Cart
+        </h2>
+        <div class="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
+          <div class="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl">
+            <div class="space-y-6">
+              <div class="rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 md:p-6">
+                {cart.map((item, index) => (
+                  <CartItem key={index} item={item} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
