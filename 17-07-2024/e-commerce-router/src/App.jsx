e0f04d4b330d7cfa2b53d396dbb795174a45870c
RobertaMi89 <roberta.miosi@gmail.com>
2024-07-17 17:09:01 +0200

    fix

--- 17-07-2024/e-commerce-router/src/App.jsx
+++ 17-07-2024/e-commerce-router/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import ProductItem from "./components/ProductItem.jsx";
-import { CartProvider } from "./components/CartContext.jsx";
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -12,16 +11,14 @@ const App = () => {
   }, []);
 
   return (
-    <CartProvider>
-      <div className="container mx-auto my-8">
-        <h1 className="text-4xl font-bold text-center mb-8">Product List</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {products.map((product) => (
-            <ProductItem key={product.id} product={product} />
-          ))}
-        </div>
+    <div className="container mx-auto my-8">
+      <h1 className="text-4xl font-bold text-center mb-8">Product List</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
       </div>
-    </CartProvider>
+    </div>
   );
 };
 
