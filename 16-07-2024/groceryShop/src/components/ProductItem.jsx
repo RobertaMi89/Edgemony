05d93c51ff0dba76ec79d13486873bec7569265c
RobertaMi89 <roberta.miosi@gmail.com>
2024-07-17 12:04:27 +0200

    style btn

--- 16-07-2024/groceryShop/src/components/ProductItem.jsx
+++ 16-07-2024/groceryShop/src/components/ProductItem.jsx
@@ -5,22 +5,22 @@ const ProductItem = ({ product }) => {
   const { addToCart } = useContext(CartContext);
 
   return (
-    <div className="border rounded-lg shadow-lg p-4 flex flex-col items-center">
-      <img
-        src={product.image}
-        alt={product.title}
-        className="object-cover mb-4"
-      />
-      <h2 className="text-xl font-semibold mb-2 text-center">
-        {product.title}
-      </h2>
-      <p className="text-gray-700 mb-4">${product.price}</p>
-      <button
-        onClick={() => addToCart(product)}
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"
-      >
-        Add to Cart
-      </button>
+    <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 relative">
+      <img src={product.image} alt={product.title} className="rounded-t-lg" />
+      <div className="p-5">
+        <h4 className="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">
+          {product.title}
+        </h4>
+        <p className="mb-9 font-normal text-gray-700 dark:text-gray-400">
+          ${product.price}
+        </p>
+        <button
+          onClick={() => addToCart(product)}
+          className="bg-blue-500 text-white w-48 px-4 py-2  rounded hover:bg-blue-700 absolute bottom-2.5 left-2.5 "
+        >
+          Add to Cart
+        </button>
+      </div>
     </div>
   );
 };
