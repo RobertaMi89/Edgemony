6df2273aab38241728fc67d9d58b6869c1638dcf
RobertaMi89 <roberta.miosi@gmail.com>
2024-07-18 19:33:10 +0200

    fix productPage and add style

--- 18-07-2024/e-commerce-router/src/components/ProductItem.jsx
+++ 18-07-2024/e-commerce-router/src/components/ProductItem.jsx
@@ -34,18 +34,19 @@ const ProductItem = ({ product }) => {
         </button>
       </div>
       <Modal isOpen={isModalOpen} OnClose={() => setIsModalOpen(false)}>
-        <img
-          src={product.image}
-          alt={product.title}
-          className="rounded-t-lg w-96 h-auto object-cover m-2 place-content-center"
-        />
-
-        <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
-          <Link to={`product/${product.id}`}>
+        <div className="flex justify-center">
+          <img
+            src={product.image}
+            alt={product.title}
+            className="rounded-t-lg w-96 h-auto object-cover m-2"
+          />
+        </div>
+        <div className="flex items-center justify-end p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+          <Link to={`/product/${product.id}`} state={{ product }}>
             <button
               data-modal-hide="static-modal"
               type="button"
-              className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+              className="py-2.5 px-5 ms-3 text-sm items-endfont-medium border-yellow-600 text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             >
               Info
             </button>
--- 18-07-2024/e-commerce-router/src/pages/ProductPage.jsx
+++ 18-07-2024/e-commerce-router/src/pages/ProductPage.jsx
@@ -1,27 +1,37 @@
-import React from "react";
+import CartContext from "../components/CartContext";
+import React, { useContext } from "react";
+import { useLocation } from "react-router-dom";
 
-function ProductPage({ product }) {
+function ProductPage() {
+  const { addToCart } = useContext(CartContext);
+  const location = useLocation();
+  const { product } = location.state;
   return (
     <>
       <div className="bg-gray-100 dark:bg-gray-800 py-8">
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 ">
           <div className="flex flex-col md:flex-row -mx-4">
-            <div className="md:flex-1 px-4">
-              <div className="h-[460px] rounded-lg bg-gray-300 dark:bg-gray-700 mb-4">
+            <div className="md:flex-1 px-4 justify-center">
+              <div className="h-[460px] rounded-lg flex justify-center dark:bg-gray-700 mb-4">
                 <img
-                  className="w-full h-full object-cover"
+                  className="rounded-t-lg h-full object-cover "
                   src={product.image}
                   alt="Product Image"
                 />
               </div>
-              <div className="flex -mx-2 mb-4">
-                <div className="w-1/2 px-2">
-                  <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">
+              <div className="flex mb-4 justify-center items-center">
+                <div className="px-2">
+                  <button
+                    onClick={() => {
+                      addToCart(product);
+                    }}
+                    className=" bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700"
+                  >
                     Add to Cart
                   </button>
                 </div>
-                <div className="w-1/2 px-2">
-                  <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">
+                <div className="px-2">
+                  <button className=" bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">
                     Add to Wishlist
                   </button>
                 </div>
@@ -92,14 +102,7 @@ function ProductPage({ product }) {
                   Product Description:
                 </span>
                 <p className="text-gray-600 dark:text-gray-300 text-sm mt-2">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  sed ante justo. Integer euismod libero id mauris malesuada
-                  tincidunt. Vivamus commodo nulla ut lorem rhoncus aliquet.
-                  Duis dapibus augue vel ipsum pretium, et venenatis sem
-                  blandit. Quisque ut erat vitae nisi ultrices placerat non eget
-                  velit. Integer ornare mi sed ipsum lacinia, non sagittis
-                  mauris blandit. Morbi fermentum libero vel nisl suscipit, nec
-                  tincidunt mi consectetur.
+                  {product.description}
                 </p>
               </div>
             </div>
