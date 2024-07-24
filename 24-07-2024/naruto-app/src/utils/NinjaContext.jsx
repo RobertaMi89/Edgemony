698580a00d8b970d8f358efe1f2110c1363d0c28
RobertaMi89 <roberta.miosi@gmail.com>
2024-07-24 18:28:26 +0200

    fix localstorage

--- 24-07-2024/naruto-app/src/utils/NinjaContext.jsx
+++ 24-07-2024/naruto-app/src/utils/NinjaContext.jsx
@@ -5,20 +5,19 @@ const NinjaContext = createContext();
 export const NinjaProvider = ({ children }) => {
   const [ninjaList, setNinjaList] = useState([]);
 
-  // Carica i dati dal localStorage all'inizio
   useEffect(() => {
-    const savedNinjas = JSON.parse(localStorage.getItem("ninjaList")) || [];
+    const savedNinjas = JSON.parse(localStorage.getItem("ninjaList"));
     setNinjaList(savedNinjas);
   }, []);
 
-  // Salva i dati nel localStorage ogni volta che ninjaList cambia
   useEffect(() => {
-    localStorage.setItem("ninjaList", JSON.stringify(ninjaList));
+    if (ninjaList.length > 0)
+      localStorage.setItem("ninjaList", JSON.stringify(ninjaList));
   }, [ninjaList]);
 
   const addNinja = (ninja) => {
     setNinjaList((prev) => {
-      const updatedList = [...prev, { ...ninja, id: Date.now() }];
+      const updatedList = [...prev, { ...ninja, id: crypto.randomUUID() }];
       localStorage.setItem("ninjaList", JSON.stringify(updatedList));
       return updatedList;
     });
