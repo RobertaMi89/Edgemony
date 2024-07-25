b242d17f097b0e53c5446279bc3a9930e5636b63
RobertaMi89 <roberta.miosi@gmail.com>
2024-07-25 16:21:20 +0200

    first commit

---
+++ 25-07-2024/book-app/src/data/labels.js
@@ -0,0 +1,18 @@
+export const labels = {
+  bookTableTitle: "Title",
+  bookTableAuthor: "Author",
+  bookTableGenre: "Genre",
+  bookTableISBN: "ISBN",
+  bookTableBtnDetail: "Details",
+
+  bookList: "Book list",
+  leggiBookTitle: "Leggi Book",
+
+  navHome: "Home",
+  filterBookByTitle: "Filter Book by Title:",
+  insertBookTitle: "Insert Book Title",
+
+  isLoading: "Is loading...",
+
+  errorHuHo: "Hu-oh",
+};
