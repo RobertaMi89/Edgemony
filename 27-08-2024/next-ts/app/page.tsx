bd763471c0424093aebdb246e89c04a475e9a134
RobertaMi89 <roberta.miosi@gmail.com>
2024-08-27 17:23:55 +0200

    add style

--- 27-08-2024/next-ts/app/page.tsx
+++ 27-08-2024/next-ts/app/page.tsx
@@ -3,8 +3,8 @@ import { harryPotterBooks } from "@/utils/books";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Wizard World</h1>
+    <main className="bg-yellow-50">
+      <h1 className="text-center font-bold text-5xl my-2 text-blue-950">Wizard World</h1>
       <div  className="flex">
       {harryPotterBooks.map(b=> <Card book={b}/>)}
       </div>
