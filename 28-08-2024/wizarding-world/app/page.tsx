76e15195d039b2e86c839863bd4e065103353c4e
RobertaMi89 <roberta.miosi@gmail.com>
2024-08-28 18:41:49 +0200

    fix styles

--- 28-08-2024/wizarding-world/app/page.tsx
+++ 28-08-2024/wizarding-world/app/page.tsx
@@ -1,4 +1,4 @@
-import Card from "@/components/card";
+import Card from "@/components/Card";
 import fetchAllMovies, { fetchAllBooks }  from "@/utils/Api";
 import { transformBookToCardItem, transformMovieToCardItem } from "@/utils/Transformers";
 
@@ -8,27 +8,31 @@ export default async function Home() {
     const movies = await fetchAllMovies();
     const books = await fetchAllBooks();
 
-    const itemsBook = [...movies.map(transformMovieToCardItem)];
-    const itemBook =[...books.map(transformBookToCardItem)];
+    const itemsMovie = [...movies.map(transformMovieToCardItem)];
+    const itemsBook =[...books.map(transformBookToCardItem)];
 
     return (
       <main className="bg-yellow-50">
         <h1 className="text-center font-bold text-5xl my-2 text-blue-950 mt-0 mb-0 p-2">
           Wizarding  World
         </h1>
-        <div className="flex flex-wrap">
+        <h2 className="text-center text-4xl my-2 text-blue-950 mt-0 mb-0 p-5">Movies</h2>
+        <div>
+        <div className="flex flex-wrap justify-center">
+          {itemsMovie.length > 0 ? (
+            itemsMovie.map((item) => <Card key={item.id} item={item} />)
+          ) : (
+            <p>No items found</p>
+          )}
+        </div>
+        <h2 className="text-center text-4xl my-2 text-blue-950 mt-0 mb-0 p-5">Books</h2>
+        <div className="flex flex-wrap justify-center">
           {itemsBook.length > 0 ? (
             itemsBook.map((item) => <Card key={item.id} item={item} />)
           ) : (
             <p>No items found</p>
           )}
         </div>
-        <div className="flex flex-wrap">
-          {itemsBook.length > 0 ? (
-            itemsBook.map((item) => <Card key={item.id} item={item} />)
-          ) : (
-            <p>No items found</p>
-          )}
         </div>
       </main>
     );
