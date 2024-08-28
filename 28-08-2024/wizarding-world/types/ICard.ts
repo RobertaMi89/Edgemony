cc3d5c875e22cf1fcc712181f675e13f92512b10
RobertaMi89 <roberta.miosi@gmail.com>
2024-08-28 18:14:45 +0200

    add interfaces

---
+++ 28-08-2024/wizarding-world/types/ICard.ts
@@ -0,0 +1,12 @@
+
+export interface ICard{
+    item:ICardItem;
+}
+
+export interface ICardItem {
+    id: string;
+    title: string;
+    coverImage: string;
+    description: string;
+    creator: string; 
+  }
