bb3d87aacf7176c63807f0d513aee163d55909d5
RobertaMi89 <roberta.miosi@gmail.com>
2024-08-29 10:03:13 +0200

    fix

--- 28-08-2024/wizarding-world/app/page.tsx
+++ 28-08-2024/wizarding-world/app/page.tsx
@@ -41,7 +41,7 @@ export default async function Home() {
     return (
       <main className="bg-yellow-50">
         <h1 className="text-center font-bold text-5xl my-2 text-blue-950">
-          Wizard World
+          Wizarding World
         </h1>
         <p>No items found</p>
       </main>
