bda4f5db1dc9890c07b73157b829870f3d0c2264
RobertaMi89 <roberta.miosi@gmail.com>
2024-08-29 17:47:28 +0200

    add style

--- 29-08-2024/wizarding-world/app/(routes)/signup/page.tsx
+++ 29-08-2024/wizarding-world/app/(routes)/signup/page.tsx
@@ -1,17 +1,16 @@
 import React from "react"
+import Image from "next/image"
+import Potion from "@/app/assets/potion.gif"
+
 export default function Signup() {
     return (
         <>
   
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-bgSignUp">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            alt="Your Company"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            className="mx-auto h-10 w-auto"
-          />
+          <Image src={Potion} alt=""/>
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Iscriviti
           </h2>
         </div>
 
@@ -19,7 +18,7 @@ export default function Signup() {
           <form action="#" method="POST" className="space-y-6">
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
+                Email 
               </label>
               <div className="mt-2">
                 <input
