662779f2f37fb8b71a87ec51dbf9368b13e67df9
RobertaMi89 <roberta.miosi@gmail.com>
2024-09-02 21:00:36 +0200

    add style

--- 30-08-2024/wizarding-world/app/(routes)/detailPage/movie/[id]/page.tsx
+++ 30-08-2024/wizarding-world/app/(routes)/detailPage/movie/[id]/page.tsx
@@ -12,13 +12,38 @@ export default async function detailPage({ params }: DetailProps) {
   const item = transformMovieToCardItem(itemRaw);
 
   return (
-    <div>
+    <>
       <div>
-        <img src={item.coverImage} alt={item.title} />
-        <h2>{item.title}</h2>
-        <p>{item.description}</p>
-        <p>Created by: {item.creator}</p>
+        <div>
+          <h2></h2>
+          <p></p>
+        </div>
       </div>
-    </div>
+
+      <section className="py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
+        <div className="max-w-screen-xl px-4 mx-auto 2xl:px-0">
+          <div className="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
+            <div className="shrink-0 max-w-md lg:max-w-lg mx-auto">
+              <img src={item.coverImage} alt={item.title} />
+            </div>
+
+            <div className="mt-6 sm:mt-8 lg:mt-0">
+              <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
+                {item.title}
+              </h1>
+              <div className="mt-4 sm:items-center sm:gap-4 sm:flex"></div>
+
+              <hr className="my-6 md:my-8 border-gray-200 dark:border-gray-800" />
+
+              <p className="mb-6 text-gray-500 dark:text-gray-400">
+                {item.description}
+              </p>
+
+              <p>{item.creator}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
