ee5e46482f643cee8ed794f575b5cf6400de971b
RobertaMi89 <roberta.miosi@gmail.com>
2024-09-03 10:07:51 +0200

    set loading

--- 30-08-2024/wizarding-world/app/loading.tsx
+++ 30-08-2024/wizarding-world/app/loading.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
-import Maps from "./assets/maps.gif"
-import Image from 'next/image'
+import React from "react";
+import Glasses from "@/app/assets/glasses.gif";
+import Image from "next/image";
 
 export default function loading() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <Image src ={Maps} alt=""/>
+      <Image src={Glasses} alt="loading" />
     </div>
-  )
+  );
 }
-
-
