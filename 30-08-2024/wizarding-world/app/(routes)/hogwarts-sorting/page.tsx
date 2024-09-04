133c7e873971c6c1c97cbd43393f1723e6bfc33f
RobertaMi89 <roberta.miosi@gmail.com>
2024-09-04 23:04:33 +0200

    add back home

--- 30-08-2024/wizarding-world/app/(routes)/hogwarts-sorting/page.tsx
+++ 30-08-2024/wizarding-world/app/(routes)/hogwarts-sorting/page.tsx
@@ -60,43 +60,63 @@ const Sorting: React.FC = () => {
   const currentQuestion: IQuestion = quiz.questions[currentQuestionIndex];
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">{quiz.title}</h1>
-      <p className="mb-4">{quiz.description}</p>
-      <div className="space-y-4">
-        <div className="border-b pb-4">
-          <h2 className="text-xl font-semibold">{currentQuestion.question}</h2>
-          <ul className="list-disc pl-5">
-            {currentQuestion.answers.map(
-              (answer: IAnswer, ansIndex: number) => {
-                const house = Object.keys(answer.points)[0];
-                const points = Object.values(answer.points)[0];
-
-                return (
-                  <li key={ansIndex}>
-                    <button
-                      onClick={() =>
-                        handleAnswer(currentQuestionIndex, house, points)
-                      }
-                      className="text-blue-500 hover:underline"
-                    >
-                      {answer.text}
-                    </button>
-                  </li>
-                );
-              }
-            )}
-          </ul>
-        </div>
-        {currentQuestionIndex === quiz.questions.length - 1 &&
-          Object.keys(answers).length > 0 && (
-            <div className="mt-4">
-              <h2 className="text-xl font-semibold">La tua casa è:</h2>
-              <p className="text-2xl font-bold">{calculateHouse()}</p>
-            </div>
-          )}
+    <>
+      <div className="self-start">
+        <a href="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-12 text-lime-700 cursor-pointer hover:text-lime-600"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-4.28 9.22a.75.75 0 0 0 0 1.06l3 3a.75.75 0 1 0 1.06-1.06l-1.72-1.72h5.69a.75.75 0 0 0 0-1.5h-5.69l1.72-1.72a.75.75 0 0 0-1.06-1.06l-3 3Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </a>
       </div>
-    </div>
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">{quiz.title}</h1>
+        <p className="mb-4">{quiz.description}</p>
+        <div className="space-y-4">
+          <div className="border-b pb-4">
+            <h2 className="text-xl font-semibold">
+              {currentQuestion.question}
+            </h2>
+            <ul className="list-disc pl-5">
+              {currentQuestion.answers.map(
+                (answer: IAnswer, ansIndex: number) => {
+                  const house = Object.keys(answer.points)[0];
+                  const points = Object.values(answer.points)[0];
+
+                  return (
+                    <li key={ansIndex}>
+                      <button
+                        onClick={() =>
+                          handleAnswer(currentQuestionIndex, house, points)
+                        }
+                        className="text-blue-500 hover:underline"
+                      >
+                        {answer.text}
+                      </button>
+                    </li>
+                  );
+                }
+              )}
+            </ul>
+          </div>
+          {currentQuestionIndex === quiz.questions.length - 1 &&
+            Object.keys(answers).length > 0 && (
+              <div className="mt-4">
+                <h2 className="text-xl font-semibold">La tua casa è:</h2>
+                <p className="text-2xl font-bold">{calculateHouse()}</p>
+              </div>
+            )}
+        </div>
+      </div>
+    </>
   );
 };
 
